bd6386273c268f960ca43668008edbb60e8cb051
Ivan Popov <popov1703.ivan@ya.ru>
2024-09-22 23:44:04 +0300

    feature-1 - добавил сервис для расчета ЗП - добавил зависимости OpenApi и Lombok - создал DTO для получения данных

--- vacation-calculator/src/main/java/ru/vacation/calculator/vacation_calculator/VacationCalculatorApplication.java
+++ vacation-calculator/src/main/java/ru/vacation/calculator/vacation_calculator/VacationCalculatorApplication.java
@@ -1,13 +1,27 @@
 package ru.vacation.calculator.vacation_calculator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Расчет заработной платы",
+                version = "1.0.0",
+                description = "API для расчета заработной платы",
+                contact = @Contact(
+                        name = "Иван Попов",
+                        email = "popov1703.ivan@ya.ru"
+                )
+        )
+)
 public class VacationCalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VacationCalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VacationCalculatorApplication.class, args);
+    }
 
 }
